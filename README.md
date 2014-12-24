03e830434b300071e6b2a7dccce08539350f701b
Sadat <sadatnazrul@gmail.com>
2014-12-24 00:53:32 -0800

    Upload search codes

---
+++ README.md
@@ -0,0 +1,13 @@
+Hello! This is a sample text for testing our
+Regular Expression code on Python. 
+
+Simply run the search.py code and you will see
+the frequency of the word 'code'.
+
+Run the code and enjoy. Notice that I am having 
+a lot of fun repeating the word 'code'.
+Code code CODE cOde coDE codE 
+CODE!
+
+OK. I will stop now. CODE.
+
---
+++ search.py
@@ -0,0 +1,29 @@
+import re
+
+file=open('README.md')
+str2search=''
+
+for message in file:
+	str2search += message 
+
+#Look for all possible combination of the word 'code'
+word='(C|c)(O|o)(D|d)(E|e)'
+compile_word=re.compile(word)
+
+#Search for 'code' on the README.md
+word_search=re.search(compile_word,str2search)
+
+#Determine frequency of the word 'code'
+word_frequency=re.findall(compile_word,str2search)
+count=0
+for word in word_frequency:
+    count += 1
+
+#Print output
+print
+print 'Word searched:'
+print(word_search.group())
+print
+print 'Frequency:'
+print str(count)
+print
